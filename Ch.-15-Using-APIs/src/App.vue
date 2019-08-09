f69a6760d8e7bf90edb797a49a1ad99c049e5beb
ivan006 <ivan.copeland2015@gmail.com>
2019-08-09 18:16:06 +0200

    1

--- Ch.-15-Using-APIs/src/App.vue
+++ Ch.-15-Using-APIs/src/App.vue
@@ -1,60 +1,72 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="container">
+    <div class="row">
+      <!-- <div class=" col-md-3">
+
+      </div> -->
+      <div class="col-xs-12 col-sm-8 sm-offset-2 col-md-6 offset-md-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label for="">Username</label>
+          <input type="text" name="" value="" class="form-control" v-model="user.username">
+
+        </div>
+        <div class="form-group">
+          <label for="">Mail</label>
+          <input type="text" name="" value="" class="form-control" v-model="user.email">
+
+        </div>
+        <button type="submit" name="button" class="btn btn-primary" @click="submit">Submit</button>
+        <hr>
+        <button type="submit" name="button" class="btn btn-primary" @click="fetchData">Get Data</button>
+        <br><br>
+        <ul class="list-group">
+          <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      user: {
+        username: '',
+        email: '',
+      },
+      users: []
+    }
+  },
+  methods: {
+    submit() {
+      this.$http.post('https://udemy-practice-vuejs-app.firebaseio.com/data.json', this.user)
+        .then(responce => {
+          console.log(responce)
+        }, error => {
+          console.log(error)
+        });
+    },
+    fetchData() {
+      this.$http.get('https://udemy-practice-vuejs-app.firebaseio.com/data.json')
+        .then(responce => {
+          return responce.json();
+        })
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+
+            console.log(resultArray);
+          this.users = resultArray;
+        });
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
