659ddb97f844ec2856fd8062b2e89bb386d50a3f
ivan006 <ivan.copeland2015@gmail.com>
2019-08-09 13:43:56 +0200

    1

--- Ch.-7-Crafty-Components/src/App.vue
+++ Ch.-7-Crafty-Components/src/App.vue
@@ -1,11 +1,32 @@
-<template lang="html">
-  <app-servers></app-servers>
-
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
+    import Header from './Shared/components/Header.vue';
+    import Footer from './Shared/components/Footer.vue';
+    import Servers from './Server/components/Servers.vue';
+    import ServerDetails from './Server/components/ServerDetails.vue';
 
+    export default {
+        components: {
+            'app-header': Header,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
 </script>
 
-<style lang="css" scoped>
+<style>
+
 </style>
