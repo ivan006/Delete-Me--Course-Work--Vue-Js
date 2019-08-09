655b8c4a8215452c28a8dba0a8a7f7d7f04a3ce9
ivan006 <ivan.copeland2015@gmail.com>
2019-08-09 23:30:59 +0200

    1

--- fontpicker-vue-2/src/main.js
+++ fontpicker-vue-2/src/main.js
@@ -1,17 +1,7 @@
-import Vue from "vue";
-import App from "./App.vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import VueMaterial from "vue-material";
-import "vue-material/dist/vue-material.min.css";
-/* eslint-disable */
-Vue.use(VueMaterial);
-Vue.use(VueAxios, axios);
-import FontPicker from "font-picker-vue";
-
-Vue.use(FontPicker);
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
 
 new Vue({
+  el: '#app',
   render: h => h(App)
-}).$mount("#app");
+})
